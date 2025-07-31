bcc8b2e38c9a36dcf757bc0b2815a85c597b5047
Guilherme <guilherandradeee@gmail.com>
2025-07-31 16:47:00 -0300

    trem

---
+++ src/game/upgrades/Estagiario.java
@@ -0,0 +1,11 @@
+package game.upgrades;
+
+import game.upgrades.interfaces.UpgradesAtivos;
+
+public class Estagiario implements UpgradesAtivos{
+    private String nome = "Estágiarios";
+    private String descricao = "";
+    private int valor = 400;
+    private int incrementoValorClique = 5;
+
+}
